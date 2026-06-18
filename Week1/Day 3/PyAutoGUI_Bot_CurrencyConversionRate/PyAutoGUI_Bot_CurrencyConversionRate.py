5460a626f59589aabe9306705c66a71efdfedfa0
Vignesh Ram <vigneshram03@users.noreply.github.com>
2026-06-18 20:51:02 +0200

    Add files via upload

---
+++ Week1/Day 3/PyAutoGUI_Bot_CurrencyConversionRate/PyAutoGUI_Bot_CurrencyConversionRate.py
@@ -0,0 +1,76 @@
+#---------------------------------------------
+# This BOT automates the process of caputuring the EUR to INR conversion rate from Google search, 
+# saving it in a Notepad file, and taking a screenshot of the Notepad file.
+#---------------------------------------------
+
+import pyautogui
+import time
+from datetime import datetime
+
+#---------------------------------------------
+# Path configuration
+#---------------------------------------------
+#file_name = "DailyCurrencyConversionReport"
+folder_path = r"C:\Users\vigne\Downloads\DailyCurrencyConversionReport"
+
+today_date = datetime.now().strftime("%Y-%m-%d")
+
+file_path = folder_path + "_" + today_date + ".txt"
+screenshot_save_path = folder_path + "_" + today_date + ".png"
+#---------------------------------------------
+# Step 1: Open Chrome browser
+#---------------------------------------------
+
+pyautogui.hotkey('win', 'r')  # Open the Run dialog
+time.sleep(1)
+pyautogui.typewrite("chrome", interval=0.1)
+pyautogui.press('enter')
+
+#---------------------------------------------
+#Step 2: Search for EUR to INR conversion rate
+#---------------------------------------------
+time.sleep(2)
+pyautogui.typewrite("EUR to INR", interval=0.1)
+pyautogui.press('enter')
+
+#---------------------------------------------
+#Step 3: Copy the conversion rate
+#---------------------------------------------
+time.sleep(3)  # Wait for the search results to load
+pyautogui.moveTo(400, 820)  # Move to the location of the conversion rate (adjust coordinates as needed)
+pyautogui.doubleClick()  # Double click to select the conversion rate
+pyautogui.hotkey('ctrl', 'c')  # Copy the selected conversion rate
+time.sleep(3)
+
+#---------------------------------------------
+#Step 4: Open Excel and paste the conversion rate
+#---------------------------------------------
+
+pyautogui.hotkey('win', 'r')  # Open the Run dialog
+time.sleep(1)
+pyautogui.typewrite("notepad", interval=0.1)
+pyautogui.press('enter')
+time.sleep(5)
+
+#pyautogui.hotkey('ctrl', 'n')  # Open a new Excel workbook
+#time.sleep(2)
+pyautogui.typewrite(datetime.now().strftime("%Y-%m-%d %H-%M-%S"), interval=0.1)  # Type the current date and time
+pyautogui.press('tab')  # Move to the next cell
+pyautogui.hotkey('ctrl', 'v')  # Paste the copied conversion rate
+pyautogui.press('tab')  # Move to the next cell
+pyautogui.press('tab')  # Move to the next cell
+pyautogui.typewrite("Conversion rate captured Successfully", interval=0.1)  
+
+pyautogui.hotkey('ctrl', 's')  # Save the Notepad file
+time.sleep(2)
+pyautogui.typewrite(file_path, interval=0.1)  # Type the Notepad file path
+pyautogui.press('enter')
+
+#---------------------------------------------
+#Step 5: Save a screenshot of the Notepad file
+#---------------------------------------------
+time.sleep(2)
+pyautogui.screenshot(screenshot_save_path)  # Save a screenshot of the Notepad file
+time.sleep(2)
+
+print("Daily currency conversion report generated and saved successfully.")
